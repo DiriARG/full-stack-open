a2aefb69c6d3c375554ccba841bd672e053c6f1a
DiriARG <matidirisio@gmail.com>
2025-08-29 13:11:15 -0300

    Ejercicio 1.5 resuelto.

--- parte-1/info-curso/src/App.jsx
+++ parte-1/info-curso/src/App.jsx
@@ -3,7 +3,7 @@ const Header = (props) => {
     <div>
       <h1>Información del curso</h1>
       <h2>Nombre del curso:</h2>
-      <p>{props.course}</p>
+      <p>{props.course.name}</p>
     </div>
   );
 };
@@ -34,27 +34,31 @@ const Total = (props) => {
   );
 };
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
   const totalEjercicios =
-    parts[0].exercises + parts[1].exercises + parts[2].exercises;
+    course.parts[0].exercises +
+    course.parts[1].exercises +
+    course.parts[2].exercises;
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
+      <Content parts={course.parts} />
       <Total totalEjercicios={totalEjercicios} />
     </div>
   );
