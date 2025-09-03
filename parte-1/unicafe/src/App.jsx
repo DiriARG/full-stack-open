f7968859760f9e3f70294dbd5262d5ef825e1e2f
DiriARG <matidirisio@gmail.com>
2025-09-03 13:29:46 -0300

    Ejercicio 1.10 resuelto.

--- parte-1/unicafe/src/App.jsx
+++ parte-1/unicafe/src/App.jsx
@@ -8,16 +8,28 @@ const Statistics = ({ good, neutral, bad, total, promedio, positivo }) => {
     return (
       <div>
         <h2>Estadísticas</h2>
-        <p>Bueno: {good}</p>
-        <p>Neutral: {neutral}</p>
-        <p>Malo: {bad}</p>
-        <p>Total: {total} </p>
-        <p>Promedio: {promedio}</p>
-        <p>Positivos: {positivo} %</p>
+        <StatisticLine texto="Bueno" valor={good} />
+        <StatisticLine texto="Neutral" valor={neutral} />
+        <StatisticLine texto="Malo" valor={bad} />
+        <StatisticLine texto="Total" valor={total} />
+        <StatisticLine texto="Promedio" valor={promedio} />
+        <StatisticLine texto="Positivos" valor={`${positivo}%`} />
       </div>
     );
   }
 };
+
+// Componente "Button" para definir los botones utilizados para enviar comentarios.
+const Button = ({ handleClick, opinion }) => (
+  <button onClick={handleClick}> {opinion}</button>
+);
+
+// Componente "StatisticLine" para mostrar una única estadística, por ejemplo, la puntuación media.
+const StatisticLine = ({ texto, valor }) => (
+  <p>
+    {texto}: {valor}
+  </p>
+);
 
 const App = () => {
   // guarda los clics de cada botón en su propio estado
@@ -27,15 +39,16 @@ const App = () => {
   const total = good + neutral + bad;
   // good +1 (voto positivo), neutral 0 (voto neutro) y bad -1 (voto negativo).
   const promedio =
-    total === 0 ? 0 : (good * 1 + neutral * 0 + bad * -1) / total; // Utilizamos condición ternaria, Si... si no...
+    total === 0 ? 0 : ((good * 1 + neutral * 0 + bad * -1) / total).toFixed(1); // Utilizamos condición ternaria, Si... si no...
   const positivo = total === 0 ? 0 : ((good / total) * 100).toFixed(1); // "toFixed(1)"" lo redonde a 1 decimal.
 
   return (
     <div>
       <h1>¿Qué te pareció nuestros productos/servicios?</h1>
-      <button onClick={() => setGood(good + 1)}>Bueno</button>
-      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
-      <button onClick={() => setBad(bad + 1)}>Malo</button>
+      <Button handleClick={() => setGood(good + 1)} opinion="Bueno" />
+      <Button handleClick={() => setNeutral(neutral + 1)} opinion="Neutral" />
+      <Button handleClick={() => setBad(bad + 1)} opinion="Malo" />
+
       <Statistics
         good={good}
         neutral={neutral}
