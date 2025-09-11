d06d6824f62af248763918a5f309c1fcc65a2025
DiriARG <matidirisio@gmail.com>
2025-09-11 12:54:51 -0300

    Ejercicio 2.9* resuelto.

--- parte-2/agenda-telefonica/src/App.jsx
+++ parte-2/agenda-telefonica/src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
   // Nuevo estado para manejar el valor del input del número de teléfono.
   const [nuevoNumero, setNuevoNumero] = useState("");
+  // Nuevo estado para guardar el término de búsqueda que el usuario escriba.
+  const [nuevaBusqueda, setNuevaBusqueda] = useState("");
 
   // Controlador de eventos para agregar nuevos nombres.
   const agregarNombre = (evento) => {
@@ -17,7 +24,8 @@ const App = () => {
     } else {
       const objetoNombre = {
         name: newName,
-        numero: nuevoNumero,
+        number: nuevoNumero,
+        id: persons.length + 1,
       };
       setPersons(persons.concat(objetoNombre));
       setNewName("");
@@ -29,16 +37,32 @@ const App = () => {
   const handleCambioDeNombre = (evento) => {
     setNewName(evento.target.value);
   };
+
   // Controlador de eventos que actualiza el estado "nuevoNumero" con el valor del input.
   const handleCambioDeNumero = (evento) => {
     setNuevoNumero(evento.target.value);
   };
+
+  // Controlador de eventos que actualiza el estado "nuevaBusqueda" con el valor del input de búsqueda.
+  const handleCambioDeBusqueda = (evento) => {
+    setNuevaBusqueda(evento.target.value);
+  };
+  
+  // Lógica para filtrar los contactos sin distinguir entre mayúsculas y minúsculas.
+  const contactosFiltrados = persons.filter((persona) =>
+    persona.name.toLowerCase().includes(nuevaBusqueda.toLowerCase())
+  );
 
   return (
     <div>
       <h2>Agenda Telefónica</h2>
       <form onSubmit={agregarNombre}>
         <div>
+          Filtro mostrado con: 
+          <input value={nuevaBusqueda} onChange={handleCambioDeBusqueda} />
+        </div>
+        <div>
+          <h2>Agregar nuevo contacto</h2>
           Nombre: <input value={newName} onChange={handleCambioDeNombre} />
         </div>
         <div>
@@ -50,10 +74,10 @@ const App = () => {
       </form>
       <h2>Números</h2>
       <ul>
-        {persons.map((persona) => (
-          <li key={persona.name}>
-            {persona.name} {persona.numero}
-          </li> // Se utiliza el nombre de la persona como valor de la propiedad "key".
+        {contactosFiltrados.map((persona) => (
+          <li key={persona.id}>
+            {persona.name} {persona.number}
+          </li>
         ))}
       </ul>
     </div>
