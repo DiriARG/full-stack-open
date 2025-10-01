51b76d9fc693a201fc0e2a74446a8c0dd3e01565
DiriARG <matidirisio@gmail.com>
2025-10-01 16:24:40 -0300

    Habilita las llamadas a la API para el ejercicio 3.11 en desarrollo local.

--- parte-2/agenda-telefonica/vite.config.js
+++ parte-2/agenda-telefonica/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // En modo desarrollo, Vite redirige todas las peticiones que comienzan con /api al servidor Express en el puerto 3001.
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+    }
+  },
 })
