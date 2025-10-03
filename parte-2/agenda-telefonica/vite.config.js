6e0d95cd33b8aec67a4794a9bb9fcdc04627d2bb
DiriARG <matidirisio@gmail.com>
2025-10-03 10:37:54 -0300

    Correción del puerto --> Ejercicio 3.13.

--- parte-2/agenda-telefonica/vite.config.js
+++ parte-2/agenda-telefonica/vite.config.js
@@ -6,9 +6,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // En modo desarrollo, Vite redirige todas las peticiones que comienzan con /api al servidor Express en el puerto 3001.
+      // En modo desarrollo, Vite redirige todas las peticiones que comienzan con /api al servidor Express en el puerto 10000.
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:10000', // Siempre tiene que apuntar al puerto donde se está ejecutando el servidor backend (Express).
         changeOrigin: true,
       },
     }
