eafc69371156ecec6ffd2dc3fc9aeaff54371425
DiriARG <matidirisio@gmail.com>
2025-10-08 14:14:58 -0300

    Ejercicio 4.4 resuelto.

--- parte-4/utils/list_helper.js
+++ parte-4/utils/list_helper.js
@@ -1,8 +1,14 @@
 // Archivo que contiene funciones auxiliares para trabajar con listas de blogs.
 const dummy = (blogs) => {
-    return 1;
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  // "reduce" realiza una suma acumulada, reduciendo el array a un solo valor, sumando los "likes" de cada "blog".
+  return blogs.reduce((suma, blog) => suma + blog.likes, 0);
 };
 
 module.exports = {
   dummy,
+  totalLikes,
 };
