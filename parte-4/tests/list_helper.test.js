39319e48f1b89cbf2feef5f5fb5eb6e93b36600a
DiriARG <matidirisio@gmail.com>
2025-10-08 15:02:33 -0300

    Ejercicio 4.5* resuelto.

--- parte-4/tests/list_helper.test.js
+++ parte-4/tests/list_helper.test.js
@@ -89,3 +89,57 @@ describe("Total de likes", () => {
     assert.strictEqual(result, 36);
   });
 });
+
+describe("Blog favorito", () => {
+  const blogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0,
+    },
+  ]
+
+  test("cuando una lista esta vacía devuelve null", () => {
+    const result = listHelper.favoriteBlog([])
+    assert.strictEqual(result, null)
+  })
+
+  test("cuando una lista tiene un solo blog devuelve ese mismo blog", () => {
+    const listWithOneBlog = [blogs[0]]
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    assert.deepStrictEqual(result, {
+      title: "React patterns",
+      author: "Michael Chan",
+      likes: 7,
+    })
+  })
+
+  test("de una lista con varios blogs, se devuelve el que tiene más likes", () => {
+    const result = listHelper.favoriteBlog(blogs)
+    assert.deepStrictEqual(result, {
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      likes: 12,
+    })
+  })
+})
+
--- parte-4/utils/list_helper.js
+++ parte-4/utils/list_helper.js
@@ -1,3 +1,5 @@
+const blog = require("../models/blog");
+
 // Archivo que contiene funciones auxiliares para trabajar con listas de blogs.
 const dummy = (blogs) => {
   return 1;
@@ -8,7 +10,28 @@ const totalLikes = (blogs) => {
   return blogs.reduce((suma, blog) => suma + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  // Verificación en caso de que la lista esté vacia para evitar errores.
+  if (blogs.length === 0) {
+    return null; // Se devuelve null porque la función tiene objetivo devolver un objeto ({ title, author, url, likes }); "null" muestra la ausencia de ese objeto esperado.
+  }
+
+  // "max" es el acumulador, el blog que tiene el máximo número de likes. "blog" es el elemento actual que se está analizando.
+  const blogMasLikeado = blogs.reduce((max, blog) =>
+    // Acá devuelve el blog con más likes de los dos (el acumulador "max" o el "blog" actual).
+    blog.likes > max.likes ? blog : max
+  );
+
+  // Se devuelve el objeto con solo las propiedades que necesitamos.
+  return {
+    title: blogMasLikeado.title,
+    author: blogMasLikeado.author,
+    likes: blogMasLikeado.likes,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
