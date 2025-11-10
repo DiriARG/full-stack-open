48e7fec689fca7b78d1f104e757dbf119035c0b6
DiriARG <matidirisio@gmail.com>
2025-11-10 12:01:04 -0300

    Ejercicio 6.3 resuelto.

--- parte-6/redux-anecdotes/src/App.jsx
+++ parte-6/redux-anecdotes/src/App.jsx
@@ -1,16 +1,21 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from "react-redux";
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector((state) => state);
+  const dispatch = useDispatch();
 
-  const vote = id => {
-    console.log('vote', id)
-  }
+  const vote = (id) => {
+    console.log("vote", id);
+    dispatch({
+      type: "VOTE",
+      payload: id,
+    });
+  };
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote => (
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
@@ -27,7 +32,7 @@ const App = () => {
         <button>create</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
