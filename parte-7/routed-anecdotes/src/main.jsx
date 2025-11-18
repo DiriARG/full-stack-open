0d5a5fa540546c09d0fde0a10c1dfc76c555bdd6
DiriARG <matidirisio@gmail.com>
2025-11-18 17:04:45 -0300

    Ejercicio 7.1 resuelto.

--- parte-7/routed-anecdotes/src/main.jsx
+++ parte-7/routed-anecdotes/src/main.jsx
@@ -1,5 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // Se envuelve toda la app en "BrowserRouter" porque es el componente que habilita el sistema de enrutamiento de React Router (Link, Routes, Route, etc).
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
