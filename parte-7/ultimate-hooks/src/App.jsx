40ed13dadcfba74eeeec31e357a4fefa4f1ccb4e
DiriARG <matidirisio@gmail.com>
2025-11-20 21:44:25 -0300

    Ejercicio 7.8 resuelto.

--- parte-7/ultimate-hooks/src/App.jsx
+++ parte-7/ultimate-hooks/src/App.jsx
@@ -15,19 +15,33 @@ const useField = (type) => {
   }
 }
 
+/* "useResource" es un hook personalizado para encapsular toda la lógica necesaria para manejar un recurso REST (como /notes o /persons) sin repetir código. 
+Es totalmente reutilizable gracias a baseUrl.
+*/
 const useResource = (baseUrl) => {
+  // Cada llamada a useResource tiene su propio estado, osea, uno para notes y otro para persons.
   const [resources, setResources] = useState([])
 
-  // ...
+  // Se cargan todos los recursos. 
+  useEffect(() => {
+    axios.get(baseUrl).then(respuesta => {
+      setResources(respuesta.data)
+    })
+  }, [baseUrl]) // Asegura que useEffect se re-ejecute si la URL de la API cambia.
 
-  const create = (resource) => {
-    // ...
+  const create = async (resource) => {
+    // Envía el recurso al backend.
+    const respuesta = await axios.post(baseUrl, resource)
+    // Se concatena el recurso nuevo al estado existente.
+    setResources(resources.concat(respuesta.data))
   }
 
   const service = {
     create
   }
 
+  /* resources: array con los elementos del recurso.
+  service: objeto con funciones, en este caso, create(). */
   return [
     resources, service
   ]
@@ -38,6 +52,19 @@ const App = () => {
   const name = useField('text')
   const number = useField('text')
 
+  /*
+    Llamar useResource 2 veces crea 2 "instancias" del hook:
+
+    1) Una para notes:
+       - resources = lista de notas
+       - service = { create() }
+
+    2) Otra para persons:
+       - resources = lista de personas
+       - service = { create() }
+
+    Cada instancia es independiente y tiene su propio estado.
+  */
   const [notes, noteService] = useResource('http://localhost:3005/notes')
   const [persons, personService] = useResource('http://localhost:3005/persons')
 
