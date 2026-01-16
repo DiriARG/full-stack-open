56730eb201e225ba65d468b6a569586ba7f672d4
DiriARG <matidirisio@gmail.com>
2026-01-16 15:14:23 -0300

    Ejercicio 12.7 resuelto.

--- parte-12/part12-containers-applications/todo-app/todo-backend/routes/todos.js
+++ parte-12/part12-containers-applications/todo-app/todo-backend/routes/todos.js
@@ -1,49 +1,63 @@
-const express = require('express');
-const { Todo } = require('../mongo')
+const express = require("express");
+const { Todo } = require("../mongo");
 const router = express.Router();
 
 /* GET todos listing. */
-router.get('/', async (_, res) => {
-  const todos = await Todo.find({})
+router.get("/", async (_, res) => {
+  const todos = await Todo.find({});
   res.send(todos);
 });
 
 /* POST todo to listing. */
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const todo = await Todo.create({
     text: req.body.text,
-    done: false
-  })
+    done: false,
+  });
   res.send(todo);
 });
 
 const singleRouter = express.Router();
 
 const findByIdMiddleware = async (req, res, next) => {
-  const { id } = req.params
-  req.todo = await Todo.findById(id)
-  if (!req.todo) return res.sendStatus(404)
+  const { id } = req.params;
+  req.todo = await Todo.findById(id);
+  if (!req.todo) return res.sendStatus(404);
 
-  next()
-}
+  next();
+};
 
 /* DELETE todo. */
-singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
+singleRouter.delete("/", async (req, res) => {
+  await req.todo.delete();
   res.sendStatus(200);
 });
 
 /* GET todo. */
-singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+singleRouter.get("/", async (req, res) => {
+  // Como el middleware "findByIdMiddleware" ya encontró la tarea y la puso en "req.todo" solo se envía como respuesta.
+  res.json(req.todo);
 });
 
 /* PUT todo. */
-singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+singleRouter.put("/", async (req, res) => {
+  const { text, done } = req.body;
+
+  // Si la propiedad "text" fue enviada en la petición, se actualiza en el documento.
+  if (text !== undefined) {
+    req.todo.text = text;
+  }
+  
+  if (done !== undefined) {
+    req.todo.done = done;
+  }
+
+  // Se guardan los cambios en MongoDB.
+  const actualizarTarea = await req.todo.save();
+  // Se muestran.
+  res.json(actualizarTarea);
 });
 
-router.use('/:id', findByIdMiddleware, singleRouter)
-
+router.use("/:id", findByIdMiddleware, singleRouter);
 
 module.exports = router;
