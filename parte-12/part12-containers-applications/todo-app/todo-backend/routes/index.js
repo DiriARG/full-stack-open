82c0622d8c260d78bf957111a4092e8360de3a85
DiriARG <matidirisio@gmail.com>
2026-01-18 10:40:09 -0300

    Ejercicio 12.16 resuelto.

--- parte-12/part12-containers-applications/todo-app/todo-backend/routes/index.js
+++ parte-12/part12-containers-applications/todo-app/todo-backend/routes/index.js
@@ -1,28 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getAsync } = require('../redis');
-const configs = require('../util/config')
+const { getAsync } = require("../redis");
+const configs = require("../util/config");
 
-let visits = 0
+let visits = 0;
 
 /* GET index data. */
-router.get('/', async (req, res) => {
-  visits++
+router.get("/", async (req, res) => {
+  visits++;
 
   res.send({
     ...configs,
-    visits
+    visits,
   });
 });
 
-router.get('/statistics', async (_req, res) => {
+router.get("/statistics", async (_req, res) => {
   // Se obtiene el contador de Redis.
-  const addedTodos = await getAsync('added_todos')
-
+  const addedTodos = await getAsync("added_todos");
+  console.log("a ver ");
   // Se envia la respuesta en el formato JSON solicitado.
   res.json({
-    added_todos: Number(addedTodos) || 0
-  })
-})
+    added_todos: Number(addedTodos) || 0,
+  });
+});
 
 module.exports = router;
